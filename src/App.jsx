6e105fe203f75f35ea13e42cb7c15cb3c03de8e4
Ne0sky <mailmeakash2@gmail.com>
2023-08-05 20:15:19 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Layout from "./Layout"
+import Contact from "./pages/Contact"
+import Home from "./pages/Home"
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+
+
+
+function App() {
+
+
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Layout/>}>
+            <Route index element={<Home/>} />
+            <Route path='/contact' element={<Contact/>}/>
+            </Route>
+        </Routes>
+      </Router>
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+const Home = () => {
+  return (
+    <div className='home'>
+        <div className="banner w-full h-[75vh]  
+        bg-no-repeat bg-cover bg-center bg-fixed 
+        bg-hero relative p-4">
+        
+        <div className='flex  items-center'>
+        <div className='h-[10vh] w-[2vw] bg-pink-900 mr-4'></div>
+        <h1 className='text-4xl font-bold text-white z-50'>
+        <p>Make your Brand</p>
+        <p>Stand Out</p>
+        </h1>
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
