e553c7d80e031f084739b5223f429b075e185929
Ne0sky <99585944+Ne0sky@users.noreply.github.com>
2023-09-25 18:10:05 +0530

    add animation to project setion of home page

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,7 +1,55 @@
 import React from 'react'
 import ProjectCard from '../components/ProjectCard'
 import { Link, NavLink } from 'react-router-dom';
+import {motion} from 'framer-motion';
 const Projects = () => {
+
+  const data=[
+    {
+      to:"/portfolio/all",
+      title:"All projects",
+      src:"/assets/Rectangle-14.avif"
+    },
+    {
+      to:"/portfolio/hospitality",
+      title:"Hospitality",
+      src:"/assets/Rectangle-15.avif"
+    },
+    {
+      to:"/portfolio/automotive",
+      title:"Automotive",
+      src:"/assets/Rectangle-16.avif"
+    },
+    {
+      to:"/portfolio/architectural",
+      title:"Architectural",
+      src:"/assets/Rectangle-17.avif"
+    },
+    {
+      to:"/portfolio/healthcare",
+      title:"Healthcare",
+      src:"/assets/Rectangle-18.avif"
+    },
+    {
+      to:"/portfolio/retail",
+      title:"Retail",
+      src:"/assets/Rectangle-19.avif"
+    },
+  ]
+
+  const fadeinAnimation ={
+    initial:{
+      opacity:0,
+      y:100
+    },
+    animate: (index)=>({
+      opacity:1,
+      y:0,
+      transition:{
+        delay:0.05*index
+      }
+    })
+  }
   return (
     <div className='py-8'>
         <p className='font-secondary text-center pt-8 text-xl uppercase'>building brands since 1998</p>
@@ -9,13 +57,21 @@ const Projects = () => {
       <p className='bg-rose-600 w-44 h-1 mb-16 mx-auto '></p>
 
       <section className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 py-8'>
+        {
+          data.map((data, index)=>{
+            return <motion.NavLink
+            variants={fadeinAnimation}
+            initial="initial"
+            whileInView="animate"
+            viewport={
+            {once:true}
+            }
+            custom={index}
 
-        <NavLink to='/portfolio/all'><ProjectCard title={'All projects'} src={`/assets/Rectangle-14.avif`}/></NavLink>
-        <NavLink to='/portfolio/hospitality'><ProjectCard title={'Hospitality'} src={`/assets/Rectangle-15.avif`}/></NavLink>
-        <NavLink to='/portfolio/automotive'><ProjectCard title={'Automotive'} src={`/assets/Rectangle-16.avif`}/></NavLink>
-        <NavLink to='/portfolio/architectural'><ProjectCard title={'Architectural'} src={`/assets/Rectangle-17.avif`}/></NavLink>
-        <NavLink to='/portfolio/healthcare'><ProjectCard title={'Healthcare'} src={`/assets/Rectangle-18.avif`}/></NavLink>
-        <NavLink to='/portfolio/retail'><ProjectCard title={'Retail'} src={`/assets/Rectangle-19.avif`}/></NavLink>
+            index={index} to={data.to}><ProjectCard title={data.title} src={data.src}/></motion.NavLink>
+          })
+        }
+        
 
       </section>
     </div>
