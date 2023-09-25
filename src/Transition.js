cd6229eba62924296b25ab99000792f3422d0911
Ne0sky <99585944+Ne0sky@users.noreply.github.com>
2023-09-25 17:55:14 +0530

    add framer motion, animation to home page, landing page and services section

---
+++ src/Transition.js
@@ -0,0 +1,7 @@
+const Transition = {
+    duration: 3,
+    
+    ease: [0.43, 0.13, 0.23, 0.96],
+  };
+  
+  export default Transition
--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,19 +1,43 @@
 import React from 'react'
 import ServiceCard from '../components/ServiceCard'
+import {motion} from 'framer-motion';
+import Transition from '../Transition'
 const Services = () => {
+
+  const fadeinAnimation ={
+    initial:{
+      opacity:0,
+      y:100
+    },
+    animate:{
+      opacity:1,
+      y:0,
+      transition:{
+        duration:1.5
+      }
+    }
+  }
   return (
-    <div>
+    <div
+    >
         
       {/* Services Section */}
       <p className='font-secondary text-center pt-8 text-xl uppercase'>What we do</p>
       <h2 className='text-center pt-2 font-semibold text-2xl md:text-4xl uppercase'>Our Services</h2>
       <p className='bg-rose-600 w-36 h-1  mx-auto '></p>
 
-      <section className="services flex flex-col md:flex-row justify-around items-center md:items-start py-16 w-screen overflow-x-hidden lg:w-3/4 mx-auto">
+      <motion.section 
+      variants={fadeinAnimation}
+      initial="initial"
+      whileInView="animate"
+      viewport={
+        {once:true}
+      }
+      className="services flex flex-col md:flex-row justify-around items-center md:items-start py-16 w-screen overflow-x-hidden lg:w-3/4 mx-auto">
         <ServiceCard title='Multi-Site Branding' desc='Ensure quality and consistency across national locations.' src={`/assets/multi-site.png`}/>
         <ServiceCard title='Architectural Branding' desc='Providing you with high specification exterior and interior architectural signage solutions' src={`/assets/architectural.png`}/>
         <ServiceCard title='Wayfinder Signage' desc='Design, manufacture and installation of wayfinding signage.' src={`/assets/directional.png`}/>
-      </section>
+      </motion.section>
     </div>
   )
 }
