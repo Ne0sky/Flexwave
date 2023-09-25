443f74f3ba31b0d20f3f3a9e9b8b3458e2b77aaf
Ne0sky <99585944+Ne0sky@users.noreply.github.com>
2023-09-25 14:18:41 +0530

    add hover effect to portfolio pictures

--- src/components/Banner_half.jsx
+++ src/components/Banner_half.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Contactbtn from '../components/Contactbtn'
-const Banner_half = () => {
+const Banner_half = ({title}) => {
   return (
     <section className="banner   w-full h-[60vh] bg-automotive flex  items-center bg-no-repeat bg-cover bg-center bg-fixed relative ">
         <div className="background-gradient absolute top-0 left-0 bg-gradient-to-r from-slate-900 to-[#0000003f] z-10 w-full h-full "></div>
@@ -10,7 +10,7 @@ const Banner_half = () => {
           <div className="h-[4rem] w-[0.2rem] md:h-[4rem] xl:h-[10rem] rounded-xl md:w-[0.5rem] bg-rose-600 mr-4 "></div>
           <h2 className="text-3xl md:text-4xl xl:text-6xl font-bold text-white">
             <p>Our Work</p>
-            <p>Automotive : Our Business <br/> Cases</p>
+            <p>{title} : Our Business <br/> Cases</p>
           </h2>
           </div>
          <div className=' flex items-end xl:py-16'>
--- src/pages/Portfolio_All.jsx
+++ src/pages/Portfolio_All.jsx
@@ -11,12 +11,12 @@ const Portfolio_All = () => {
     const[file, setFile] = useState(null)
     return (
       <div className='font-primary overflow-x-hidden '>
-        <Banner_half/>
+        <Banner_half title='All'/>
         <Navbar_portfolio/>
         <div className="h-[1px] w-3/4 rounded-xl mx-auto bg-zinc-900 hidden md:flex mb-5"></div>
           <div className='w-full columns-1 md:columns-2 lg:columns-3 space-y-5 gap-5 p-5 '>
           {AutomotiveSrc.map((data, index)=>{
-              return  <img onClick={()=>setFile(data)} key={index} className='rounded-xl object-cover w-full cursor-pointer h-auto shadow-lg' src={data.path} alt="Porfolio_img" />
+              return  <div className='overflow-hidden rounded-xl'><img onClick={()=>setFile(data)} key={index} className='overflow-hidden  transition-all ease-in-out duration-500 hover:scale-105  object-cover w-full cursor-pointer h-auto shadow-lg' src={data.path} alt="Porfolio_img" /></div>
           })}
           </div>
           <div className={'popup-media transition-all' } style={{display: file?'block':'none'}}>
