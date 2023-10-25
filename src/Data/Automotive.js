85e21b8013da7695b9db912b5bf48ee4e80f1d38
Ne0sky <99585944+Ne0sky@users.noreply.github.com>
2023-10-25 17:46:58 +0530

    fix naming error in hospitality data

--- src/Data/Automotive.js
+++ src/Data/Automotive.js
@@ -14,7 +14,7 @@ const AutomotiveSrc = [
     desc:'ACP Signage',
     location:'Kharagpur'
     },
-    {path:'/assets/Automotive/4.jpeg',
+    {path:'/assets/Automotive/4.jpg',
     title:'Sonalike Tructors',
     desc:'ACP Signage',
     location:'Kharagpur'
